bdd12beff4b92c2a1d0e735558498b95bb597ac2
gio87 <gio.dz@icloud.com>
2024-01-31 16:05:21 -0500

    add hompages input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,83 @@
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-type FormValues = {
+type InputsForm = {
   firstName: string;
   lastName: string;
+  email: string;
+  number: string;
+  aboutMe: string;
 };
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm<InputsForm>();
+  const onSubmit: SubmitHandler<InputsForm> = (data) => console.log(data);
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form
+        className="flex flex-col items-center justify-between"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div>
+          <label>First Name</label>
+          <input
+            type="text"
+            {...register("firstName", {
+              required: "Please enter first name.",
+            })}
+          />
+          {errors.firstName && <p>{errors.firstName.message}</p>}
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input
+            type="text"
+            {...register("lastName", { required: "Please enter last name." })}
+          />
+          {errors.lastName && <p>{errors.lastName.message}</p>}
+        </div>
+
+        <div className="flex flex-col items-start justify-between ">
+          <label>About Me</label>
+          <textarea
+            {...register("aboutMe", {
+              required: "Please provide information about yourself.",
+            })}
+          />
+          {errors.aboutMe && <p>{errors.aboutMe.message}</p>}
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            {...register("email", {
+              required: "Please enter email address.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          {errors.email && <p>{errors.email.message} </p>}
+        </div>
+
+        <div>
+          <label>Phone Number</label>
+          <input
+            type="text"
+            {...register("number", { required: "Please enter phone number." })}
+          />
+          {errors.number && <p>{errors.number.message}</p>}
+        </div>
+
+        <button className="w-40 h-12 bg-[#6B40E3] text-white" type="submit">
+          Next
+        </button>
       </form>
     </div>
   );
